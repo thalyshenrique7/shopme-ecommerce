22e283538a922f8df8b0a8b76dbe0236f4cd4579
thalyshenrique7 <thalys_henrique@outlook.com>
2023-08-19 08:20:17 -0300

    New Features
    Create New User Function
    - Show List Roles
    - Save User
    - Message Success
    - Cancel Button

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 
 @Controller
@@ -19,8 +22,30 @@ public class UserController {
 	public String findAll(Model model) {
 		List<User> users = service.findAll();
 		model.addAttribute("users", users);
-		
+
 		return "users";
+	}
+
+	@GetMapping("/users/new")
+	public String newUser(Model model) {
+		User user = new User();
+		List<Role> listRoles = service.findRoles();
+
+		user.setEnabled(true);
+		model.addAttribute("user", user);
+		model.addAttribute("listRoles", listRoles);
+
+		return "user_form";
+	}
+
+	@PostMapping("/users/save")
+	public String saveUser(User user, RedirectAttributes redirectAttributes) {
+		System.out.println(user);
+		service.save(user);
+		
+		redirectAttributes.addFlashAttribute("message", "Usuário salvo com sucesso!");
+		
+		return "redirect:/users";
 	}
 
 }
--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 
 @Service
@@ -13,8 +14,19 @@ public class UserService {
 	@Autowired
 	private UserRepository repo;
 
+	@Autowired
+	private RoleRepository roleRepo;
+
 	public List<User> findAll() {
 		return (List<User>) repo.findAll();
+	}
+
+	public List<Role> findRoles() {
+		return (List<Role>) roleRepo.findAll();
+	}
+
+	public void save(User user) {
+		repo.save(user);
 	}
 
 }
